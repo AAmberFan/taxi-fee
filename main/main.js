3a4bcab2b298d539f25ca6a8c8d4ed9b3ea50702
AAmberFan <filuwan@gmail.com>
2018-04-09 13:39:30 -0400

    edit1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,30 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(miles, waitTime) {
+    let summary = `公里数：${miles}公里，等待时间：${waitTime}分钟，`;
+    let milePrice = calculateMilePrice(miles);
+    let waitPrice = calculateWaitPrice(waitTime);
+    let price = Math.round(milePrice+waitPrice);
+    summary+=`共花费：${price}元\n`
+    return summary;
 };
+function calculateMilePrice(miles) {
+    let milePrice = 0;
+    if(miles <=2 ){
+        milePrice= isLess2Miles(miles);
+    }else if(miles<=8){
+        milePrice = isLess8Miles(miles)；
+    }else{
+        milePrice = isMoreThan8Miles(miles);
+    }
+}
+function isLess2Miles(miles) {
+    return 6;
+}
+function isLess8Miles(miles) {
+    return 6 + (miles-2)*0.8;
+}
+function  isMoreThan8Miles(miles) {
+    return 10.4 + (miles - 8)*0.8*1.5;
+}
+function calculateWaitPrice(waitTime){
+    return waitTime*0.25;
+}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,30 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('the taxi fee when have mile and waitTime ', function () {
+        let miles = 2;
+        let waitTime = 0;
+
+        let result = main(miles, waitTime).trim();
+        let expected ='公里数：2公里，等待时间：0分钟，共花费：6元\n'
+        expect(result).toEqual(expected)
+    });
+    it('the taxi fee when have mile and waitTime ', function () {
+        let miles = 3;
+        let waitTime = 1;
+
+        let result = main(miles, waitTime).trim();
+        let expected ='公里数：3公里，等待时间：1分钟，共花费：7元\n'
+        expect(result).toEqual(expected)
+    });
+    it('the taxi fee when have mile and waitTime ', function () {
+        let miles = 20;
+        let waitTime = 0;
+
+        let result = main(miles, waitTime).trim();
+        let expected ='公里数：3公里，等待时间：0分钟，共花费：25元\n'
+        expect(result).toEqual(expected)
+    });
+
+
 });
